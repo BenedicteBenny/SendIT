fff20b8895c56dc2cd527b9aa055cd7074297fa5
benedictemusabimana@gmail.com <musabene1@yahoo.com>
2018-11-22 16:51:24 +0200

    Add models folder

--- Server/app.js
+++ Server/app.js
@@ -3,6 +3,7 @@ import express from 'express'
 import routers from './routes'
 const app= express();
 app.use(express.json())
+
 const body=require('body-parser').json()
 app.use('/api/v1/', routers)
 const port= process.env.PORT || 3000
---
+++ Server/models/parcels.js
@@ -0,0 +1,22 @@
+const parcels=[{
+	id: 1,
+	parcelName: 'Tshirts',
+	from: 'Kigali',
+	to: 'Musanze',
+	weight: 4
+				},
+	{id: 2,
+	parcelName: 'Laptop',
+	from: 'Kigali',
+	to: 'Musanze',
+	weight: 2
+			},
+	{
+	id: 3,
+	parcelName: 'Water boxes',
+	from: 'Huye',
+	to: 'Rulindo',
+	weight: 15
+	}]
+
+	export default parcels;
